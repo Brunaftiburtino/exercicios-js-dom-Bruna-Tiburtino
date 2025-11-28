077c6c45438c734d19eb9fed58afaa365773e88f
Bruna Tiburtino <107516018+Brunaftiburtino@users.noreply.github.com>
2025-11-28 11:02:47 -0300

    Add files via upload

---
+++ Exercícios_JS_DOM/Ex02/questao02.js
@@ -0,0 +1,15 @@
+function mudarForma() {
+    
+    const forma = document.getElementById("forma");
+
+    const botao = document.getElementById("btn-forma");
+
+    if (forma.style.borderRadius === "50%") {
+        forma.style.borderRadius = "0%";
+        botao.innerText = "Mudar forma";
+
+    } else {
+        forma.style.borderRadius = "50%";
+        botao.innerText = "Retornar forma original";
+    }
+}
